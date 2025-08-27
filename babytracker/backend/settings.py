07068f114bd30943f7e0a08b74a40fc2d2d5c491
Khosiyat <hosiyatsabirova@gmail.com>
2025-08-27 08:14:11 +0200

    settings are updated

--- babytracker/backend/settings.py
+++ babytracker/backend/settings.py
@@ -1,5 +1,24 @@
 INSTALLED_APPS = [
-    ...
     'rest_framework',
-    'core',
+    'rest_framework_simplejwt',
+    'corsheaders',
 ]
+
+
+
+
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    # ... other middleware
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React frontend origin
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
