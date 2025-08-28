1a25619546ba6f2ec0be8cf872be224ab7ed3489
Khosiyat <hosiyatsabirova@gmail.com>
2025-08-28 12:58:29 +0200

    signup api url is added

--- babytracker/backend/core/urls.py
+++ babytracker/backend/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import FoodItemViewSet, FeedingRecordViewSet, baby_daily_summary
+from users.views import SignupView
 
 router = DefaultRouter()
 router.register(r'food-items', FoodItemViewSet)
@@ -9,5 +10,6 @@ router.register(r'feedings', FeedingRecordViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('baby/<int:baby_id>/summary/', baby_daily_summary),
+    path('api/signup/', SignupView.as_view(), name='signup'),
     
 ]
