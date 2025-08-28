acab1cb6fcad05a1ce23f514fab2fd4c154411d6
Khosiyat <hosiyatsabirova@gmail.com>
2025-08-28 12:56:24 +0200

    signup api is added

---
+++ babytracker/backend/users/views.py
@@ -0,0 +1,21 @@
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+from rest_framework.serializers import ModelSerializer
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class SignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = UserSerializer
