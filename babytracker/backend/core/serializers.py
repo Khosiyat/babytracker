1cb3425912356db17dc65d94191622926567b343
Khosiyat <hosiyatsabirova@gmail.com>
2025-09-08 08:15:53 +0200

    serializers are updated

--- babytracker/backend/core/serializers.py
+++ babytracker/backend/core/serializers.py
@@ -19,3 +19,21 @@ class DailySummarySerializer(serializers.Serializer):
     total_calories = serializers.FloatField()
     total_protein = serializers.FloatField()
     breakdown = serializers.DictField()
+
+
+from rest_framework import serializers
+
+class DailyNutrientSummarySerializer(serializers.Serializer):
+    calories = serializers.FloatField()
+    protein = serializers.FloatField()
+    fat = serializers.FloatField()
+    carbs = serializers.FloatField()
+    vitamin_a = serializers.FloatField()
+    vitamin_c = serializers.FloatField()
+    
+    calories_pct = serializers.FloatField()
+    protein_pct = serializers.FloatField()
+    fat_pct = serializers.FloatField()
+    carbs_pct = serializers.FloatField()
+    vitamin_a_pct = serializers.FloatField()
+    vitamin_c_pct = serializers.FloatField()
